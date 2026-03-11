a358c3f30f29b8f3809fb7fa84c2c2536d7f43e4
itsfaraz <er.farazsheikh@gmail.com>
2026-03-11 14:03:00 +0530

    fic: default font style change

--- app/src/main/java/com/designlife/justdo/common/data/datastore/AppStore.kt
+++ app/src/main/java/com/designlife/justdo/common/data/datastore/AppStore.kt
@@ -6,17 +6,17 @@ import android.content.Context
 import androidx.annotation.Keep
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
-import com.designlife.justdo.settings.presentation.enums.AppTheme
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Keep
+@InternalSerializationApi @Keep
 @Serializable
 data class AppStore(
     @SerialName("todoId") val todoId : Int = -1,
     @SerialName("selectedScreen") val selectedScreen : Int = 0,
     @SerialName("appTheme") val selectedAppTheme: Int = 0,
-    @SerialName("fontSize") val selectedFontSize: Int = 0,
+    @SerialName("fontSize") val selectedFontSize: Int = 1,
     @SerialName("listItemHeight") val selectedListItemHeight: Int = 0
 ){
     @Keep
@@ -33,4 +33,5 @@ data class AppStore(
     }
 }
 
+@OptIn(InternalSerializationApi::class)
 val Context.appStore by dataStore<AppStore>("app-store.json", AppStoreSerializer)
